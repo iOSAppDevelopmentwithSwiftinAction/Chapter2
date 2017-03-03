7b03cc75affd093c7d280d8ca08563dad7ab114a
Craig Grummitt <craiggrummitt@gmail.com>
2017-03-02 23:05:06 -0500

    Revision of Chapter 2

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: # Functions
+
+import UIKit
+//: ## Declaring and calling a function
+func serve(drink: String, customer: String) -> String {
+    return("\(customer), your \(drink) is served")
+}
+print( serve(drink: "beer", customer: "Billy") )
+
+//: ## Modifying external parameter name
+func serve2(drink: String, to customer: String) -> String {
+    return("\(customer), your \(drink) is served")
+}
+print( serve2(drink: "beer", to: "Billy") )
+
+//: ## Omitting external parameter name
+func serve3(_ drink: String, to customer: String) -> String {
+    return("\(customer), your \(drink) is served")
+}
+print( serve3("beer", to: "Billy") )
+
+//: ## Default parameter name
+func serve4(_ drink: String = "Beer", to customer: String = "Billy") -> String {
+    return("\(customer), your \(drink) is served")
+}
+print( serve4() )
---
+++ HigherOrderFunctions.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: # Collection Higher Order Functions
+//:
+import UIKit
+
+var prices = [3, 1.99, 2, 1.99, 1.70]
+var updatedPrices: [Double] = []
+for var price in prices {
+    updatedPrices.append(price * 1.1)
+}
+//: ### Map with function
+func updatePrice(price: Double) -> Double {
+    return price * 1.1
+}
+var updatedPrices2 = prices.map(updatePrice)
+//: ### Map with closure
+var updatedPrices3 = prices.map( { $0 * 2 } )
+//: ### Filter
+var filteredPrices = prices.filter( { $0 >= 5 })
+//: ### Reduce
+var totalPrice = prices.reduce(0,{$0 + $1})
+//: ### Sort
+var sortedPrices = prices.sorted(by:{ $0 > $1 } )
